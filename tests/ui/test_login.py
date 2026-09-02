81fa07be2a15ee43bd2d2336429849682c55d003
blskumar <blskumar@outlook.in>
2026-09-02 09:08:35 +0530

    Updated usersjson and test_login

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -17,17 +17,4 @@ def test_login_with_valid_registered_user(page, base_url, credentials):
 
     login_page.assert_logged_in()
 
-
-@pytest.mark.parametrize(
-    "login_case",
-    invalid_login_cases(),
-    ids=lambda login_case: login_case["name"],
-)
-def test_login_rejects_invalid_and_edge_case_credentials(page, base_url, login_case):
-    login_page = LoginPage(page, base_url)
-
-    login_page.open()
-    login_page.login(login_case["username"], login_case["password"])
-
-    login_page.assert_login_error(login_case["expected_error"])
-    assert not page.get_by_text("Accounts Overview", exact=True).is_visible()
+?
